22c276acd8c5464f6dfd3fd8b7a7111ccd0c5032
farmerboy95 <jerry.stone.manutd@gmail.com>
2021-02-05 18:52:55 +0800

    ALC-20: test rest_framework

--- algocourse/urls.py
+++ algocourse/urls.py
@@ -40,5 +40,6 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ]
